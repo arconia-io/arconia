24eab5a201c26f460abc3abc484769dd80921b88
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2025-05-01 09:00:40 +0530

    Adding a new dev-services module for MySQL.
    Signed-off-by: K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>

---
+++ arconia-dev/arconia-dev-services/arconia-dev-services-mysql/src/main/java/io/arconia/dev/services/mysql/MySqlDevServicesAutoConfiguration.java
@@ -0,0 +1,65 @@
+package io.arconia.dev.services.mysql;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.devtools.restart.RestartScope;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnectionAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import io.arconia.dev.services.mysql.MySqlDevServicesAutoConfiguration.ConfigurationWithRestart;
+import io.arconia.dev.services.mysql.MySqlDevServicesAutoConfiguration.ConfigurationWithoutRestart;
+
+/**
+ * Auto-configuration for MySQL Dev Services.
+ */
+@AutoConfiguration(before = ServiceConnectionAutoConfiguration.class)
+@ConditionalOnProperty(prefix = MySqlDevServicesProperties.CONFIG_PREFIX, name = "enabled", havingValue = "true", matchIfMissing = true)
+@EnableConfigurationProperties(MySqlDevServicesProperties.class)
+@Import({ConfigurationWithRestart.class, ConfigurationWithoutRestart.class})
+public class MySqlDevServicesAutoConfiguration {
+
+    public static final String COMPATIBLE_IMAGE_NAME = "mysql";
+
+    @Configuration(proxyBeanMethods = false)
+    @ConditionalOnClass(RestartScope.class)
+    public static class ConfigurationWithRestart {
+
+        @Bean
+        @RestartScope
+        @ServiceConnection
+        @ConditionalOnMissingBean
+        MySQLContainer<?> mysqlContainer(MySqlDevServicesProperties properties) {
+            return new MySQLContainer<>(DockerImageName.parse(properties.getImageName())
+                    .asCompatibleSubstituteFor(COMPATIBLE_IMAGE_NAME))
+                    .withEnv(properties.getEnvironment())
+                    .withReuse(properties.isReusable());
+        }
+
+    }
+
+    @Configuration(proxyBeanMethods = false)
+    @ConditionalOnMissingClass("org.springframework.boot.devtools.restart.RestartScope")
+    public static class ConfigurationWithoutRestart {
+
+        @Bean
+        @ServiceConnection
+        @ConditionalOnMissingBean
+        MySQLContainer<?> mysqlContainerNoRestartScope(MySqlDevServicesProperties properties) {
+            return new MySQLContainer<>(DockerImageName.parse(properties.getImageName())
+                    .asCompatibleSubstituteFor(COMPATIBLE_IMAGE_NAME))
+                    .withEnv(properties.getEnvironment())
+                    .withReuse(properties.isReusable());
+        }
+
+    }
+
+}
---
+++ arconia-dev/arconia-dev-services/arconia-dev-services-mysql/src/test/java/io/arconia/dev/services/mysql/MySqlDevServicesAutoConfigurationTests.java
@@ -0,0 +1,55 @@
+package io.arconia.dev.services.mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.devtools.restart.RestartScope;
+import org.springframework.boot.test.context.FilteredClassLoader;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.testcontainers.containers.MySQLContainer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit tests for {@link MySqlDevServicesAutoConfiguration}.
+ */
+class MySqlDevServicesAutoConfigurationTests {
+
+    private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+            .withClassLoader(new FilteredClassLoader(RestartScope.class))
+            .withConfiguration(AutoConfigurations.of(MySqlDevServicesAutoConfiguration.class));
+
+    @Test
+    void autoConfigurationNotActivatedWhenDisabled() {
+        contextRunner
+            .withPropertyValues("arconia.dev.services.mysql.enabled=false")
+            .run(context -> assertThat(context).doesNotHaveBean(MySQLContainer.class));
+    }
+
+    @Test
+    void mysqlContainerAvailableWithDefaultConfiguration() {
+        contextRunner.run(context -> {
+            assertThat(context).hasSingleBean(MySQLContainer.class);
+            MySQLContainer<?> container = context.getBean(MySQLContainer.class);
+            assertThat(container.getDockerImageName()).contains("mysql");
+            assertThat(container.isShouldBeReused()).isFalse();
+        });
+    }
+
+    @Test
+    void mysqlContainerConfigurationApplied() {
+        contextRunner
+            .withPropertyValues(
+                "arconia.dev.services.mysql.image-name=docker.io/mysql",
+                "arconia.dev.services.mysql.environment.MYSQL_USER=test",
+                "arconia.dev.services.mysql.reusable=false"
+            )
+            .run(context -> {
+                assertThat(context).hasSingleBean(MySQLContainer.class);
+                MySQLContainer<?> container = context.getBean(MySQLContainer.class);
+                assertThat(container.getDockerImageName()).contains("docker.io/mysql");
+                assertThat(container.getEnv()).contains("MYSQL_USER=test");
+                assertThat(container.isShouldBeReused()).isFalse();
+            });
+    }
+
+}
