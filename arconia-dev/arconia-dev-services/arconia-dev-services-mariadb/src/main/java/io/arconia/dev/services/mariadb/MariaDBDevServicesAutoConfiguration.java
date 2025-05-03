73fab279da0263518665255b9f61a3d0448496e1
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2025-05-03 06:30:06 +0530

    feat(dev): Adding a new dev-services module for MariaDB.
    Signed-off-by: K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>

---
+++ arconia-dev/arconia-dev-services/arconia-dev-services-mariadb/src/main/java/io/arconia/dev/services/mariadb/MariaDBDevServicesAutoConfiguration.java
@@ -0,0 +1,65 @@
+package io.arconia.dev.services.mariadb;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.devtools.restart.RestartScope;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnectionAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import io.arconia.dev.services.mariadb.MariaDBDevServicesAutoConfiguration.ConfigurationWithRestart;
+import io.arconia.dev.services.mariadb.MariaDBDevServicesAutoConfiguration.ConfigurationWithoutRestart;
+
+/**
+ * Auto-configuration for MariaDB Dev Services.
+ */
+@AutoConfiguration(before = ServiceConnectionAutoConfiguration.class)
+@ConditionalOnProperty(prefix = MariaDBDevServicesProperties.CONFIG_PREFIX, name = "enabled", havingValue = "true", matchIfMissing = true)
+@EnableConfigurationProperties(MariaDBDevServicesProperties.class)
+@Import({ConfigurationWithRestart.class, ConfigurationWithoutRestart.class})
+public class MariaDBDevServicesAutoConfiguration {
+
+    public static final String COMPATIBLE_IMAGE_NAME = "mariadb";
+
+    @Configuration(proxyBeanMethods = false)
+    @ConditionalOnClass(RestartScope.class)
+    public static class ConfigurationWithRestart {
+
+        @Bean
+        @RestartScope
+        @ServiceConnection
+        @ConditionalOnMissingBean
+        MariaDBContainer<?> mariadbContainer(MariaDBDevServicesProperties properties) {
+            return new MariaDBContainer<>(DockerImageName.parse(properties.getImageName())
+                    .asCompatibleSubstituteFor(COMPATIBLE_IMAGE_NAME))
+                    .withEnv(properties.getEnvironment())
+                    .withReuse(properties.isReusable());
+        }
+
+    }
+
+    @Configuration(proxyBeanMethods = false)
+    @ConditionalOnMissingClass("org.springframework.boot.devtools.restart.RestartScope")
+    public static class ConfigurationWithoutRestart {
+
+        @Bean
+        @ServiceConnection
+        @ConditionalOnMissingBean
+        MariaDBContainer<?> mariadbContainerNoRestartScope(MariaDBDevServicesProperties properties) {
+            return new MariaDBContainer<>(DockerImageName.parse(properties.getImageName())
+                    .asCompatibleSubstituteFor(COMPATIBLE_IMAGE_NAME))
+                    .withEnv(properties.getEnvironment())
+                    .withReuse(properties.isReusable());
+        }
+
+    }
+
+}
---
+++ arconia-dev/arconia-dev-services/arconia-dev-services-mariadb/src/main/java/io/arconia/dev/services/mariadb/MariaDBDevServicesProperties.java
@@ -0,0 +1,74 @@
+package io.arconia.dev.services.mariadb;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import io.arconia.dev.services.core.config.DevServicesProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Properties for the MariaDB Dev Services.
+ */
+@ConfigurationProperties(prefix = MariaDBDevServicesProperties.CONFIG_PREFIX)
+public class MariaDBDevServicesProperties implements DevServicesProperties {
+
+    public static final String CONFIG_PREFIX = "arconia.dev.services.mariadb";
+
+    /**
+     * Whether the dev service is enabled.
+     */
+    private boolean enabled = true;
+
+    /**
+     * Full name of the container image used in the dev service.
+     */
+    private String imageName = "mariadb:11.7.2";
+
+    /**
+     * Environment variables to set in the container.
+     */
+    private Map<String,String> environment = new HashMap<>();
+
+    /**
+     * Whether the container used in the dev service is reusable across applications.
+     */
+    private boolean reusable = false;
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    @Override
+    public Map<String, String> getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(Map<String, String> environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public boolean isReusable() {
+        return reusable;
+    }
+
+    public void setReusable(boolean reusable) {
+        this.reusable = reusable;
+    }
+
+}
