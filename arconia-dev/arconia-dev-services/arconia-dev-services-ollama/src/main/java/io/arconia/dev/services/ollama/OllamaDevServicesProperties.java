333d8316ecbbf32deaf10360c1d5ba745aadc96f
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-05-08 00:55:28 +0100

    deps(dev): Update Ollama 0.6.6 to 0.6.7

--- arconia-dev/arconia-dev-services/arconia-dev-services-ollama/src/main/java/io/arconia/dev/services/ollama/OllamaDevServicesProperties.java
+++ arconia-dev/arconia-dev-services/arconia-dev-services-ollama/src/main/java/io/arconia/dev/services/ollama/OllamaDevServicesProperties.java
@@ -23,7 +23,7 @@ public class OllamaDevServicesProperties implements DevServicesProperties {
     /**
      * Full name of the container image used in the dev service.
      */
-    private String imageName = "ollama/ollama:0.6.6";
+    private String imageName = "ollama/ollama:0.6.7";
 
     /**
      * Environment variables to set in the container.
