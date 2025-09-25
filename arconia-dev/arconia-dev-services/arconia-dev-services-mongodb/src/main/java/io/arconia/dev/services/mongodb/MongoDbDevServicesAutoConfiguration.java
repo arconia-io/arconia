ca17f882e64405a8abe5c585e004f41fc81dc1bd
Bs Mahi <bs.mahi@gmail.com>
2025-09-25 23:24:48 +0200

    feat(dev): Add MongoDB Dev Services
    Signed-off-by: Bs Mahi <bs.mahi@gmail.com>

---
+++ arconia-dev/arconia-dev-services/arconia-dev-services-mongodb/src/main/java/io/arconia/dev/services/mongodb/MongoDbDevServicesAutoConfiguration.java
@@ -0,0 +1,67 @@
+package io.arconia.dev.services.mongodb;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.devtools.restart.RestartScope;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnectionAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import io.arconia.dev.services.mongodb.MongoDbDevServicesAutoConfiguration.ConfigurationWithRestart;
+import io.arconia.dev.services.mongodb.MongoDbDevServicesAutoConfiguration.ConfigurationWithoutRestart;
+
+/**
+ * Autoconfiguration for MongoDB Dev Services.
+ */
+@AutoConfiguration(before = ServiceConnectionAutoConfiguration.class)
+@ConditionalOnProperty(prefix = "arconia.dev.services.mongodb", name = "enabled", havingValue = "true", matchIfMissing = true)
+@EnableConfigurationProperties(MongoDbDevServicesProperties.class)
+@Import({ConfigurationWithRestart.class, ConfigurationWithoutRestart.class})
+public final class MongoDbDevServicesAutoConfiguration {
+
+    public static final String COMPATIBLE_IMAGE_NAME = "mongo";
+
+    @Configuration(proxyBeanMethods = false)
+    @ConditionalOnClass(RestartScope.class)
+    public static final class ConfigurationWithRestart {
+
+        @Bean
+        @RestartScope
+        @ServiceConnection
+        @ConditionalOnMissingBean
+        MongoDBContainer mongoDBContainer(MongoDbDevServicesProperties properties) {
+            return new MongoDBContainer(DockerImageName.parse(properties.getImageName())
+                    .asCompatibleSubstituteFor(COMPATIBLE_IMAGE_NAME))
+                    .withEnv(properties.getEnvironment())
+                    .withStartupTimeout(properties.getStartupTimeout())
+                    .withReuse(properties.getShared().asBoolean());
+        }
+
+    }
+
+    @Configuration(proxyBeanMethods = false)
+    @ConditionalOnMissingClass("org.springframework.boot.devtools.restart.RestartScope")
+    public static final class ConfigurationWithoutRestart {
+
+        @Bean
+        @ServiceConnection
+        @ConditionalOnMissingBean
+        MongoDBContainer mongoDBContainerNoRestartScope(MongoDbDevServicesProperties properties) {
+            return new MongoDBContainer(DockerImageName.parse(properties.getImageName())
+                    .asCompatibleSubstituteFor(COMPATIBLE_IMAGE_NAME))
+                    .withEnv(properties.getEnvironment())
+                    .withStartupTimeout(properties.getStartupTimeout())
+                    .withReuse(properties.getShared().asBoolean());
+        }
+
+    }
+
+}
