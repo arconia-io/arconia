e1bd4902007e7a6ca67234ec21d79bd9d43e1340
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-10-25 20:49:05 +0200

    chore(dev): Update Phoenix 11.38 to 12.7 as default image
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- arconia-observability/arconia-opentelemetry/src/test/java/io/arconia/opentelemetry/testcontainers/Images.java
+++ arconia-observability/arconia-opentelemetry/src/test/java/io/arconia/opentelemetry/testcontainers/Images.java
@@ -7,7 +7,7 @@ public final class Images {
 
     public static final String LGTM = "grafana/otel-lgtm:0.11.9";
 
-    public static final String PHOENIX = "arizephoenix/phoenix:version-11.38.0-nonroot";
+    public static final String PHOENIX = "arizephoenix/phoenix:version-12.7-nonroot";
 
     private Images() {
     }
