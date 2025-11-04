f91d38b9cc5af73e687facae6e2cc3253f8d2c51
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-11-04 21:01:30 +0100

    chore(dev): Update Grafana LGTM 0.11.14 to 0.11.15 as default image
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- arconia-dev/arconia-dev-services/arconia-dev-services-lgtm/src/main/java/io/arconia/dev/services/lgtm/LgtmDevServicesProperties.java
+++ arconia-dev/arconia-dev-services/arconia-dev-services-lgtm/src/main/java/io/arconia/dev/services/lgtm/LgtmDevServicesProperties.java
@@ -22,7 +22,7 @@ public class LgtmDevServicesProperties implements DevServicesProperties {
     /**
      * Full name of the container image used in the dev service.
      */
-    private String imageName = "grafana/otel-lgtm:0.11.14";
+    private String imageName = "grafana/otel-lgtm:0.11.15";
 
     /**
      * Environment variables to set in the service.
