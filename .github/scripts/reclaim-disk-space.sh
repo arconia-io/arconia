02122fb886470cdcf0ca613a92b8511490e5613c
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-12-14 08:53:03 +0100

    build: Optimize disk space on GH runners
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

---
+++ .github/scripts/reclaim-disk-space.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Remove .NET SDKs
+sudo rm -rf /usr/share/dotnet
+# Remove Swift toolchain
+sudo rm -rf /usr/share/swift
+# Remove Haskell (GHC)
+sudo rm -rf /opt/ghc
+sudo rm -rf /usr/local/.ghcup
+# Remove Julia
+sudo rm -rf /usr/local/julia*
+# Remove Miniconda
+sudo rm -rf /usr/share/miniconda
+# Remove Python
+sudo rm -rf /opt/pipx
+# Remove Rust
+sudo rm -rf /usr/share/rust
+# Remove Android SDKs
+sudo rm -rf /usr/local/lib/android
+# Remove Chromium
+sudo rm -rf /usr/local/share/chromium
+# Remove Microsoft/Edge and Google Chrome
+sudo rm -rf /opt/microsoft /opt/google
+# Remove Google Cloud SDK
+sudo rm -rf /usr/lib/google-cloud-sdk
+# Remove Azure CLI
+sudo rm -rf /opt/az
+# Remove PowerShell
+sudo rm -rf /usr/local/share/powershell
+
+# Prune Docker
+docker system prune -af
+docker builder prune -af
