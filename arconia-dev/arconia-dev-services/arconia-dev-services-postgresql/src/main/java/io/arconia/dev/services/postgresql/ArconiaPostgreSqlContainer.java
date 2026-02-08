350d533f7832941ce83707d143bc1dc9fbc59de9
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2026-02-08 23:16:18 +0100

    fix(dev): Handle upstream bug in PostgreSqlContainer
    See: https://github.com/testcontainers/testcontainers-java/issues/4799
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- arconia-dev/arconia-dev-services/arconia-dev-services-postgresql/src/main/java/io/arconia/dev/services/postgresql/ArconiaPostgreSqlContainer.java
+++ arconia-dev/arconia-dev-services/arconia-dev-services-postgresql/src/main/java/io/arconia/dev/services/postgresql/ArconiaPostgreSqlContainer.java
@@ -1,5 +1,6 @@
 package io.arconia.dev.services.postgresql;
 
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -15,9 +16,17 @@ final class ArconiaPostgreSqlContainer extends PostgreSQLContainer {
 
     static final String COMPATIBLE_IMAGE_NAME = "postgres";
 
+    static final String READY_REGEX = ".*database system is ready to accept connections.*\\s";
+    static final String SKIPPING_INITIALIZATION_REGEX = ".*PostgreSQL Database directory appears to contain a database; Skipping initialization:*\\s";
+
     public ArconiaPostgreSqlContainer(PostgresqlDevServicesProperties properties) {
         super(DockerImageName.parse(properties.getImageName()).asCompatibleSubstituteFor(COMPATIBLE_IMAGE_NAME));
         this.properties = properties;
+
+        // See: https://github.com/testcontainers/testcontainers-java/issues/4799
+        this.waitingFor(Wait
+                .forLogMessage("(" + READY_REGEX + ")?(" + SKIPPING_INITIALIZATION_REGEX + ")?", 2)
+        );
 
         ContainerConfigurer.base(this, properties);
         ContainerConfigurer.jdbc(this, properties);
