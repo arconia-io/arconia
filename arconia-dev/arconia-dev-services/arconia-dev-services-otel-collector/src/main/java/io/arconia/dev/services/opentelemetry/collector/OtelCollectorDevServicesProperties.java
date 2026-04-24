493595af1b8863fda8de9557399c8baf0e96492c
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2026-04-24 19:43:29 +0200

    chore(dev): Update OTEL Collector 0.149.0 to 0.150.1 as the default image
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- arconia-dev/arconia-dev-services/arconia-dev-services-otel-collector/src/main/java/io/arconia/dev/services/opentelemetry/collector/OtelCollectorDevServicesProperties.java
+++ arconia-dev/arconia-dev-services/arconia-dev-services-otel-collector/src/main/java/io/arconia/dev/services/opentelemetry/collector/OtelCollectorDevServicesProperties.java
@@ -28,7 +28,7 @@ public class OtelCollectorDevServicesProperties implements BaseDevServicesProper
     /**
      * Full name of the container image used in the dev service.
      */
-    private String imageName = "otel/opentelemetry-collector-contrib:0.149.0";
+    private String imageName = "otel/opentelemetry-collector-contrib:0.150.1";
 
     /**
      * Environment variables to set in the service.
--- arconia-observability/arconia-opentelemetry/src/test/java/io/arconia/opentelemetry/testcontainers/Images.java
+++ arconia-observability/arconia-opentelemetry/src/test/java/io/arconia/opentelemetry/testcontainers/Images.java
@@ -7,7 +7,7 @@ public final class Images {
 
     public static final String LGTM = "grafana/otel-lgtm:0.26.0";
 
-    public static final String OTEL_COLLECTOR = "otel/opentelemetry-collector-contrib:0.149.0";
+    public static final String OTEL_COLLECTOR = "otel/opentelemetry-collector-contrib:0.150.1";
 
     public static final String PHOENIX = "arizephoenix/phoenix:version-14.12.0-nonroot";
 
