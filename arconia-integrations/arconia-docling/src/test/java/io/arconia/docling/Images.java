689835c447f5d8614c965dc51af94c03f7c6fc52
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2026-05-13 07:11:40 +0200

    chore(dev): Update Docling 1.17.0 to 1.18.0 as default image
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- arconia-integrations/arconia-docling/src/test/java/io/arconia/docling/Images.java
+++ arconia-integrations/arconia-docling/src/test/java/io/arconia/docling/Images.java
@@ -5,7 +5,7 @@ package io.arconia.docling;
  */
 public class Images {
 
-    public static final String DOCLING = "ghcr.io/docling-project/docling-serve:v1.17.0";
+    public static final String DOCLING = "ghcr.io/docling-project/docling-serve:v1.18.0";
 
     private Images() {
     }
