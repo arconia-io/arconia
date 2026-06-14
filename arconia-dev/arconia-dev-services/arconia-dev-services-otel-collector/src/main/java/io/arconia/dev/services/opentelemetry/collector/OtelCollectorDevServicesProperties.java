d7e097466c4a212edc579708ad0c930ff4d5ed29
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2026-06-14 10:54:50 +0200

    chore(dev): Update OTel Collector 0.153.0 to 0.154.0 as default image
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- arconia-dev/arconia-dev-services/arconia-dev-services-otel-collector/src/main/java/io/arconia/dev/services/opentelemetry/collector/OtelCollectorDevServicesProperties.java
+++ arconia-dev/arconia-dev-services/arconia-dev-services-otel-collector/src/main/java/io/arconia/dev/services/opentelemetry/collector/OtelCollectorDevServicesProperties.java
@@ -28,7 +28,7 @@ public class OtelCollectorDevServicesProperties implements BaseDevServicesProper
     /**
      * Full name of the container image used in the dev service.
      */
-    private String imageName = "otel/opentelemetry-collector-contrib:0.153.0";
+    private String imageName = "otel/opentelemetry-collector-contrib:0.154.0";
 
     /**
      * Environment variables to set in the service.
