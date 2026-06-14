79e5625f6a83ef35a825eda99a14b1e840b4d27c
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2026-06-14 10:52:29 +0200

    chore(dev): Update Ollama 0.30.7 to 0.30.8 as default image
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- arconia-dev/arconia-dev-services/arconia-dev-services-ollama/src/main/java/io/arconia/dev/services/ollama/OllamaDevServicesProperties.java
+++ arconia-dev/arconia-dev-services/arconia-dev-services-ollama/src/main/java/io/arconia/dev/services/ollama/OllamaDevServicesProperties.java
@@ -28,7 +28,7 @@ public class OllamaDevServicesProperties implements BaseDevServicesProperties {
     /**
      * Full name of the container image used in the dev service.
      */
-    private String imageName = "ollama/ollama:0.30.7";
+    private String imageName = "ollama/ollama:0.30.8";
 
     /**
      * Environment variables to set in the service.
