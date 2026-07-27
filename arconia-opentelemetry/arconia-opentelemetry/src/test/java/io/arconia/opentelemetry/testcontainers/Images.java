1754e19e50d071fc5930b717a0dfbd33e03472c3
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2026-07-27 21:57:19 +0200

    chore(dev): Update OTel Collector 0.154.0 to 0.157.0 as default image
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- arconia-opentelemetry/arconia-opentelemetry/src/test/java/io/arconia/opentelemetry/testcontainers/Images.java
+++ arconia-opentelemetry/arconia-opentelemetry/src/test/java/io/arconia/opentelemetry/testcontainers/Images.java
@@ -9,7 +9,7 @@ public final class Images {
 
     public static final String LGTM = "grafana/otel-lgtm:0.29.2";
 
-    public static final String OTEL_COLLECTOR = "otel/opentelemetry-collector-contrib:0.154.0";
+    public static final String OTEL_COLLECTOR = "otel/opentelemetry-collector-contrib:0.157.0";
 
     public static final String OPENLIT = "ghcr.io/openlit/openlit:1.24.1";
 
