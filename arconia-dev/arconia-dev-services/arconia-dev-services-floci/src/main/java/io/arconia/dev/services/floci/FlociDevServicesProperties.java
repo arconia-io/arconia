1578466a41cbee73d060028a61ffaf5bcdeb718b
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2026-08-13 07:56:25 +0200

    chore(dev): Update Floci 1.5.33 to 1.6.0 as default image
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- arconia-dev/arconia-dev-services/arconia-dev-services-floci/src/main/java/io/arconia/dev/services/floci/FlociDevServicesProperties.java
+++ arconia-dev/arconia-dev-services/arconia-dev-services-floci/src/main/java/io/arconia/dev/services/floci/FlociDevServicesProperties.java
@@ -28,7 +28,7 @@ public class FlociDevServicesProperties implements BaseDevServicesProperties {
     /**
      * Full name of the container image used in the dev service.
      */
-    private String imageName = "floci/floci:1.5.33";
+    private String imageName = "floci/floci:1.6.0";
 
     /**
      * Environment variables to set in the service.
