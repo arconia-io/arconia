7ea3c5ead0dcd856bc920d463ad5d083055b248a
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2026-08-13 08:21:11 +0200

    chore(dev): Use Docling 1.28.0 as default image
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- arconia-dev/arconia-dev-services/arconia-dev-services-docling/src/main/java/io/arconia/dev/services/docling/DoclingDevServicesProperties.java
+++ arconia-dev/arconia-dev-services/arconia-dev-services-docling/src/main/java/io/arconia/dev/services/docling/DoclingDevServicesProperties.java
@@ -29,7 +29,7 @@ public class DoclingDevServicesProperties implements SharedDevServicesProperties
     /**
      * Full name of the container image used in the dev service.
      */
-    private String imageName = "ghcr.io/docling-project/docling-serve:v1.30.0";
+    private String imageName = "ghcr.io/docling-project/docling-serve:v1.28.0";
 
     /**
      * Environment variables to set in the service.
